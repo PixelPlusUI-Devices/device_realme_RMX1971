f23a97f080bebdda26a4327ee1baae69b84b0ff9
Adrian Roos <roosa@google.com>
2018-08-20 16:13:04 +0200

    RMX1971: Add overlay for masking the notch
    Bug: 112876936
    Test: Go to Settings > Developer Options > Simulate display with cutout > "No cutout". Cutout should be hidden. Rotate screen, take screenshots, screenrecord should all work.
    Change-Id: I66684d8254fd3c1ee6851e67c0b09d176d4c02af
    (cherry picked from commit dcc48dc9071c37c8b9fe7caee96585d20d149060)
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- device.mk
+++ device.mk
@@ -63,6 +63,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@1.0-impl.realme_sdm710
 
+# Overlays
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Vndk
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
